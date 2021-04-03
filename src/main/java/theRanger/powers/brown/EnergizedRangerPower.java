0b6d00a07fcae322393ba8af4c76cd580d4ba519
YerayBB <yeraybosoms@gmail.com>
2021-04-03 19:25:47 +0200

    Added all the powers, not 100% done, and with 0 tests, but added

---
+++ src/main/java/theRanger/powers/brown/EnergizedRangerPower.java
@@ -0,0 +1,81 @@
+package theRanger.powers.brown;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import theRanger.DefaultMod;
+import theRanger.util.TextureLoader;
+
+import static theRanger.DefaultMod.makePowerPath;
+
+//Gain 1 dex for the turn for each card played.
+
+public class EnergizedRangerPower extends AbstractPower {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = DefaultMod.makeID("EnergizedRangerPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public EnergizedRangerPower(final AbstractCreature owner, final int energyAmt) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = energyAmt;
+        if (this.amount >= 999) {
+            this.amount = 999;
+        }
+
+        type = PowerType.BUFF;
+        isTurnBased = true;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public void stackPower(int stackAmount) {
+        super.stackPower(stackAmount);
+        if (this.amount >= 999) {
+            this.amount = 999;
+        }
+
+    }
+    @Override
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+        } else {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[2];
+        }
+
+    }
+
+    @Override
+    public void onEnergyRecharge() {
+        this.flash();
+        AbstractDungeon.player.gainEnergy(this.amount);
+        this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));
+    }
+}
